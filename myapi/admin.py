421132bc4624e1169004f3bc59b6d8ebed4f9761
ender <69122294+endercoder23@users.noreply.github.com>
2020-08-09 19:54:05 +0530

    Add files via upload

---
+++ myapi/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import members
+
+admin.site.register(members)
+# Register your models here.
---
+++ myapi/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class myapiConfig(AppConfig):
+    name = 'myapi'
